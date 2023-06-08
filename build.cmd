5938eb2ffbb965b75cf346d0c1e6a02dfc053f5c
Igor Velikorossov <igor.velikorossov@microsoft.com>
2023-06-08 14:14:12 +1000

    Re-enable code coverage reporting
    Each build leg (i.e., Windows and Ubuntu) produce their respective code
    coverage reports, which are then get merged by `CodeCoverageReport`
    build job. This job then generates and publishes an HTML report.
    
    NB: The code coverage validation and enforcement are still pending and
    will be enabled later.

--- build.cmd
+++ build.cmd
@@ -24,9 +24,11 @@ EXIT /b %ERRORLEVEL%
     SET DOTNET_ROOT=%~dp0.dotnet
     :: This tells .NET Core not to go looking for .NET Core in other places
     SET DOTNET_MULTILEVEL_LOOKUP=0
+    SET PATH=%DOTNET_ROOT%;%PATH%
 
-    dotnet dotnet-coverage collect --settings ./eng/CodeCoverage.config --output ./artifacts/TestResults/ "build.cmd -test -bl"
-    dotnet reportgenerator -reports:./artifacts/TestResults/*.cobertura.xml -targetdir:./artifacts/TestResults/CoverageResultsHtml -reporttypes:HtmlInline_AzurePipelines
-    start ./artifacts/TestResults/CoverageResultsHtml/index.html
-    powershell -ExecutionPolicy ByPass -NoProfile -command "./scripts/ValidateProjectCoverage.ps1 -CoberturaReportXml ./artifacts/TestResults/.cobertura.xml"
+    SET TestResultPath=./artifacts/TestResults/Debug/
+    dotnet dotnet-coverage collect --settings ./eng/CodeCoverage.config --output %TestResultPath% "build.cmd -test -bl"
+    dotnet reportgenerator -reports:%TestResultPath%*.cobertura.xml -targetdir:%TestResultPath%CoverageResultsHtml -reporttypes:HtmlInline_AzurePipelines
+    :: start %TestResultPath%CoverageResultsHtml/index.html
+    :: powershell -ExecutionPolicy ByPass -NoProfile -command "./scripts/ValidateProjectCoverage.ps1 -CoberturaReportXml %TestResultPath%.cobertura.xml"
     EXIT /b %ErrorLevel%
